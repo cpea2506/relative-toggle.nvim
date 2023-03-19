cdc1bd49636c6fbc55fc7b0839e59a14f1de4215
CPea <cpea2506@gmail.com>
2023-03-19 12:55:37 +0700

    fix: keep the combination of `number` and `relativenumber` (#10)

--- lua/relative-toggle/init.lua
+++ lua/relative-toggle/init.lua
@@ -14,26 +14,34 @@ local function create_autocmd(event, opts)
     end
 end
 
+-- to keep the combination of number and relativenumber
+local current_number = vim.o.number
+
 ---@param relative boolean #whether relativenumber should be set
 ---@param redraw boolean #whether to redraw the screen
 local function set_relativenumber(relative, redraw)
+    -- ignore for buffer or window with these off by default
+    if not vim.o.number and not vim.o.relativenumber then
+        return
+    end
+
     local in_insert_mode = vim.api.nvim_get_mode().mode == "i"
 
-    if vim.o.number then
-        vim.opt.relativenumber = relative and not in_insert_mode
+    vim.opt.number = not relative or in_insert_mode or current_number
+    vim.opt.relativenumber = relative and not in_insert_mode
 
-        if redraw then
-            vim.cmd "redraw"
-        end
+    if redraw then
+        vim.cmd "redraw"
     end
 end
 
 function M.setup(user_config)
-    -- load user config
-    config:extend(user_config)
-
     local events = config.events
     local augroup = vim.api.nvim_create_augroup("relative-toggle", {})
+
+    vim.opt.relativenumber = true
+
+    config:extend(user_config)
 
     create_autocmd(events.on, {
         pattern = config.pattern,

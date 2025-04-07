8c9db7eeb9c84f0b018504ccd8f112970a717bb2
CPea <cpea2506@gmail.com>
2025-04-07 22:13:47 +0700

    fix: options cannot be set by user

--- lua/relative-toggle/init.lua
+++ lua/relative-toggle/init.lua
@@ -1,26 +1,13 @@
 local M = {}
 
-local config = require "relative-toggle.config"
-local logs = require "relative-toggle.logs"
-
----creates an autocommand event handler.
----@param event object
----@param opts? table<string, any>
-local function create_autocmd(event, opts)
-    local event_ok, error = pcall(vim.api.nvim_create_autocmd, event, opts)
-
-    if not event_ok then
-        logs.error(error)
-    end
-end
-
--- to keep the combination of number and relativenumber
+-- To keep the combination of number and relativenumber
 local current_number = vim.o.number
 
----@param relative boolean #whether relativenumber should be set
----@param redraw boolean #whether to redraw the screen
+---Set relative number
+---@param relative boolean #Whether relative number should be set
+---@param redraw boolean #Whether to redraw the screen
 local function set_relativenumber(relative, redraw)
-    -- ignore for buffer or window with these off by default
+    -- Ignore for buffer or window with number and relative number off by default
     if not vim.o.number and not vim.o.relativenumber then
         return
     end
@@ -31,33 +18,33 @@ local function set_relativenumber(relative, redraw)
     vim.opt.relativenumber = relative and not in_insert_mode
 
     if redraw then
-        vim.cmd "redraw"
+        vim.cmd.redraw()
     end
 end
 
 function M.setup(user_config)
-    local events = config.events
-    local augroup = vim.api.nvim_create_augroup("relative-toggle", {})
+    local config = require "relative-toggle.config"
+    local augroup = vim.api.nvim_create_augroup("relative-toggle", { clear = true })
 
     vim.opt.relativenumber = true
 
     config:extend(user_config)
 
-    create_autocmd(events.on, {
+    vim.api.nvim_create_autocmd(config.events.on, {
         pattern = config.pattern,
         group = augroup,
-        desc = "turn relative number on",
-        callback = function(ev)
-            set_relativenumber(true, ev.event == "CmdlineEnter")
+        desc = "Turn relative number on",
+        callback = function(args)
+            set_relativenumber(true, args.event == "CmdlineEnter")
         end,
     })
 
-    create_autocmd(events.off, {
+    vim.api.nvim_create_autocmd(config.events.off, {
         pattern = config.pattern,
         group = augroup,
-        desc = "turn relative number off",
-        callback = function(ev)
-            set_relativenumber(false, ev.event == "CmdlineEnter")
+        desc = "Turn relative number off",
+        callback = function(args)
+            set_relativenumber(false, args.event == "CmdlineEnter")
         end,
     })
 end

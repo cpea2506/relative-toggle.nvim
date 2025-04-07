5b29cb0822869f9632aeee9cfeb209dc1db7b96f
PinkLea <cpea2506@gmail.com>
2025-04-08 02:12:54 +0700

    refactor: cleanup config module (#17)

--- lua/relative-toggle/config.lua
+++ lua/relative-toggle/config.lua
@@ -1,54 +1,66 @@
-local logs = require "relative-toggle.logs"
-
 local config = {}
 
-config.options = {
-    ---@type string|table #pattern(s) where toggle should be enable, see: [autocmd-pattern]
+---@class events
+---@field on string|string[] #Event(s) to enable relative number, see: [autocmd-events]
+---@field off string|string[] #Event(s) to disable relative number, see: [autocmd-events]
+
+---@class options
+---@field pattern string|string[] #Pattern(s) where toggling is enabled, see: [autocmd-pattern]
+---@field events events #Event(s) that toggle between relative and absolute line numbers
+local defaults = {
     pattern = "*",
-    ---@type table<string, string | string[] > #event(s) to toggle between relative and absolute line numbers.
-    ---• `on`: event to turn relative number on
-    ---• `off`: event to turn relative number off
-    ---see: [autocmd-events]
     events = {
         on = { "BufEnter", "FocusGained", "InsertLeave", "WinEnter", "CmdlineLeave" },
         off = { "BufLeave", "FocusLost", "InsertEnter", "WinLeave", "CmdlineEnter" },
     },
 }
 
----Extend default with user config
----@param user_config table #user's defined config
-function config:extend(user_config)
-    if not user_config or vim.tbl_isempty(user_config) then
+---@type options
+local options = vim.deepcopy(defaults)
+
+---Extend default with user's config
+---@param opts options
+function config.extend(opts)
+    if not opts or vim.tbl_isempty(opts) then
         return
     end
 
-    local events = user_config.events
+    local events = opts.events
 
     if events then
+        local logs = require "relative-toggle.logs"
+
         if not (events.on and events.off) then
-            logs.error "config: events must contain two keys 'on' and 'off'"
+            logs.error("config: events must contain two keys %q and %q", "on", "off")
 
             return
         end
 
-        local types_ok, err = pcall(vim.validate, {
-            on = { events.on, { "table", "string" } },
-            off = { events.off, { "table", "string" } },
-        })
+        local function validate_events(name)
+            local type_ok, err =
+                pcall(vim.validate, "config(events." .. name .. ")", events[name], { "table", "string" })
 
-        if not types_ok then
-            logs.error(err)
+            if not type_ok then
+                logs.error(err)
 
+                return false
+            end
+
+            return true
+        end
+
+        if not validate_events "on" or not validate_events "off" then
             return
         end
     end
 
-    self.options = vim.tbl_deep_extend("force", self.options, user_config)
+    options = vim.tbl_deep_extend("force", options, opts)
 end
 
--- allow index options without options field
-return setmetatable(config, {
-    __index = function(table, key)
-        return table.options[key]
+setmetatable(config, {
+    __index = function(_, k)
+        return options[k]
     end,
 })
+
+return config
